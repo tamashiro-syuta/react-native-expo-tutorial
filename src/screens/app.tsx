5212571070e57d53e79c6ace2c3ba8162e33dc3a
tamashiro-syuta <syuta5825@gmail.com>
2024-12-29 14:12:58 +0900

    Imageコンポーネントを追加して画像を表示

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,14 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  View,
+  type ImageSourcePropType,
+} from "react-native";
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
+
+const PlaceholderImage =
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require("@/assets/images/background-image.png") as ImageSourcePropType;
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#fff" }}>Hello, World!</Text>
-      <Text style={styles.text}>
-        Open up App.tsx to start working on your app!
-      </Text>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,10 +27,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
 
